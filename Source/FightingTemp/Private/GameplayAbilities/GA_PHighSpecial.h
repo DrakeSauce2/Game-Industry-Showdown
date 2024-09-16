226484b58363f0f0da54debcdf5844d8bc0dccef
Manuel Rodriguez <mgrodri5@student.uiwtx.edu>
2024-09-16 10:03:55 -0500

    Special basics work, and other codes in the works

---
+++ Source/FightingTemp/Private/GameplayAbilities/GA_PHighSpecial.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilities/GA_AbilityBase.h"
+#include "GA_PHighSpecial.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UGA_PHighSpecial : public UGA_AbilityBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FightingTemp/Private/GameplayAbilities/GA_PLowSpecial.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilities/GA_AbilityBase.h"
+#include "GA_PLowSpecial.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UGA_PLowSpecial : public UGA_AbilityBase
+{
+	GENERATED_BODY()
+	
+};
