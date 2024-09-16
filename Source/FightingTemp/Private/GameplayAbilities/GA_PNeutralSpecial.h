9fde963413f5118cd8c8d0d76dc5ace2be61b6f7
Manuel Rodriguez <mgrodri5@student.uiwtx.edu>
2024-09-16 09:00:28 -0500

    Neutral Special being worked on

---
+++ Source/FightingTemp/Private/GameplayAbilities/GA_PNeutralSpecial.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilities/GA_AbilityBase.h"
+#include "GA_PNeutralSpecial.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UGA_PNeutralSpecial : public UGA_AbilityBase
+{
+	GENERATED_BODY()
+	UGA_PNeutralSpecial();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	TSubclassOf<class UGameplayEffect> DamageEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Anim")
+	UAnimMontage* DamageMontage;
+
+	UFUNCTION()
+	void HandleDamage(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void TryCommitSpecial(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void AbilityInputPressed(float TimeWaited);
+
+	FName SpecialName;
+
+	bool bSpecialCommitted;
+
+	void SetupWaitInputTask();
+};
